344d6c6443c299e722fd724b93f6e67e797768ad
Elena Kononchuk <eva.kononchuk@gmail.com>
2023-08-13 22:18:25 +0300

    added searching form

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,28 @@ import React from "react";
 
 export default function Weather() {
     return (
-        <p>
-            Hello World
-        </p>
+        <div>
+        <form id="city-form">
+            <div class="row">
+              <div class="col search">
+                <input
+                  type="text"
+                  placeholder="Type a city..."
+                  class="form-control"
+                  id="city-search"
+                />
+              </div>
+              <div class="col-3">
+                <input
+                  type="submit"
+                  id="search"
+                  value="Search"
+                  class="btn btn-primary"
+                />
+              </div>
+            </div>
+          </form>
+          <input src="./backspace.svg" type="image" id="backspace" alt='backspace' />
+          </div>
     );
 }
